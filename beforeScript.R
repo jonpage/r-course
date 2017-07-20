df1b1268b143cf6ea12d0d0d1528ea3c26a71519
Jonathan Page <jonpage.econ@gmail.com>
2017-07-20 13:47:47 -1000

    completed spatial lesson

--- beforeScript.R
+++ beforeScript.R
@@ -1,2 +1,2 @@
 rm(list = ls(all = TRUE))
-knitr::opts_chunk$set(message = FALSE, warning = FALSE, error = FALSE)
+knitr::opts_chunk$set(message = FALSE, warning = FALSE, error = FALSE, cache = TRUE)
