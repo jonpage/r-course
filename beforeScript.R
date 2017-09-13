e3ddde236b8f31575de125b522631b449f77d42c
Jonathan Page <jonpage.econ@gmail.com>
2017-09-13 09:23:05 -1000

    updates

--- beforeScript.R
+++ beforeScript.R
@@ -1,4 +1,2 @@
 rm(list = ls(all = TRUE))
-if("package:broom" %in% search()) detach("package:broom", unload=TRUE)
-if("package:dplyr" %in% search()) detach("package:dplyr", unload=TRUE)
 knitr::opts_chunk$set(message = FALSE, warning = FALSE, error = FALSE, cache = TRUE)
